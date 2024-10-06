c2e795eb563301eba6c86a8edbdc1b9c7549b21a
Yash gupta <johndoe@yopmail.com>
2024-10-06 11:16:17 +0530

    INLINE STYLE USING styles, URL STYLES, MULTIPLE CSS FILES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //INLINE STYLE USING styles
+  styles: ['.unique-card{background: red}', '.card-header{color:yellow; padding:19px}'],
+  //------->URL STYLES
+  // styleUrls: ['./app.component.css'],
+  //----->MULTIPLE CSS FILES 
+  // styleUrls: ['./app.component.css', '.test.component.css']
 })
 export class AppComponent {
   title = 'angular-ekart';

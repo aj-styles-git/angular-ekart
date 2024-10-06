40ed632ea6e2c6baafb47a63e6306f6a775ccd7d
Yash gupta <johndoe@yopmail.com>
2024-10-06 13:13:46 +0530

    types_of_component_selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //HTML TAG  <app-root></app-root>
   selector: 'app-root',
+  // //---->HTML ATTRIBUTE <div app-root></div>
+  // selector: ['app-root'],
+  // //---->CSS CLASS OR USING #ID <div class="app-root"></div>
+  // selector: '.app-root',
   templateUrl: './app.component.html',
   //INLINE STYLE USING styles
-  styles: ['.unique-card{background: red}', '.card-header{color:yellow; padding:19px}'],
-  //------->URL STYLES
-  // styleUrls: ['./app.component.css'],
-  //----->MULTIPLE CSS FILES 
-  // styleUrls: ['./app.component.css', '.test.component.css']
+  styles: ['.unique-card{background: red}', '.card-header{color:yellow; padding:19px}']
 })
 export class AppComponent {
   title = 'angular-ekart';

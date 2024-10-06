2699ba76cc4ca4597cf43e94b16ea47feb10dd35
Yash gupta <johndoe@yopmail.com>
2024-10-06 13:16:06 +0530

    clear branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //INLINE STYLE USING styles
-  styles: ['.unique-card{background: red}', '.card-header{color:yellow; padding:19px}'],
-  //------->URL STYLES
-  // styleUrls: ['./app.component.css'],
-  //----->MULTIPLE CSS FILES 
-  // styleUrls: ['./app.component.css', '.test.component.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-ekart';

0ca4f9fa8de89b190f7ff5238f18f12b6768ccea
Yash gupta <johndoe@yopmail.com>
2024-10-17 23:15:59 +0530

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { JeeMainsFormComponent } from './jee-mains-form/jee-mains-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JeeMainsFormComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    FormsModule
+ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
